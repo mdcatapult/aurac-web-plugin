dd835baec795f73a8077715ffe6da28928c22fe6
Nick Etherington <nick.etherington@md.catapult.org.uk>
2020-11-26 13:27:13 +0000

    Fixes failing tests.

--- src/app/router/router.component.spec.ts
+++ src/app/router/router.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RouterComponent } from './router.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('RouterComponent', () => {
   let component: RouterComponent;
@@ -8,6 +10,7 @@ describe('RouterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
       declarations: [ RouterComponent ]
     })
     .compileComponents();
