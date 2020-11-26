12ee0d5603c9f2f32d66a11b68cf5a30ca158ef9
Nick Etherington <nick.etherington@md.catapult.org.uk>
2020-11-26 13:31:02 +0000

    Disables tslint no-console rule for console.debug and console.info.

--- src/app/background/logger/logger.component.ts
+++ src/app/background/logger/logger.component.ts
@@ -10,7 +10,9 @@ export class LoggerComponent implements OnInit {
   constructor(private receiver: LogReceiverService) { }
 
   ngOnInit(): void {
+    // tslint:disable-next-line:no-console
     this.receiver.debug$.subscribe(msg => console.debug(msg));
+    // tslint:disable-next-line:no-console
     this.receiver.info$.subscribe(msg => console.info(msg));
     this.receiver.log$.subscribe(msg => console.log(msg));
     this.receiver.warn$.subscribe(msg => console.warn(msg));
