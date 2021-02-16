e9d93b8ff5c49288ecbc5ce8c389e93d572a23b9
Nick Etherington <nick.etherington@md.catapult.org.uk>
2021-02-16 14:09:09 +0000

    Adds rule from custom-webpack.config.js into webpack.config.js.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,22 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["babel-preset-env"],
+            plugins: [
+              [
+                "babel-plugin-transform-runtime",
+                { polyfill: false, regenerator: true }
+              ]
+            ]
+          }
+        }
+      },
+      {
         test: /\.scss$/,
         loader: 'postcss-loader',
         options: {
