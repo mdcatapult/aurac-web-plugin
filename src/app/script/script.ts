59ddc0272d8a848769654c3f534a74f58c745ec4
Nick Etherington <nick.etherington@md.catapult.org.uk>
2021-02-22 11:47:31 +0000

    WIP: matches inner entity

--- src/app/script/script.ts
+++ src/app/script/script.ts
@@ -73,12 +73,16 @@
 
       switch (event.type) {
         case 'mouseenter':
-          element.appendChild(span);
+          element.childNodes.forEach(childNode => {
+            if (childNode.className === 'ferret-highlight') {
+              return;
+            }
+            element.appendChild(span);
+          });
           break;
         case 'mouseleave':
           // remove ALL ferret tooltips - this catches a case such as 'Glucans biosynthesis protein D' in which both the full term and
           // 'protein' are recognised entities after NER'ing the page
-          // TODO: handle overlapping tooltips in cases where more than one entity is matched in a single phrase
           Array.from(document.getElementsByClassName('ferret-tooltip')).forEach(tooltip => tooltip.remove());
           break;
       }
