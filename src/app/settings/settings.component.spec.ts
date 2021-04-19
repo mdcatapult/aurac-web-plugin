63d016ad2c65a383fd77b280b4aaf5a046dc323c
Michael Sweeton <michael.sweeton@md.catapult.org.uk>
2021-04-19 16:48:10 +0100

    wip

--- src/app/settings/settings.component.spec.ts
+++ src/app/settings/settings.component.spec.ts
@@ -1,27 +1,35 @@
-// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-//
-// import {HttpClientTestingModule} from '@angular/common/http/testing';
-// import {RouterTestingModule} from '@angular/router/testing';
-//
-// describe('RouterComponent', () => {
-//   let component: RouterComponent;
-//   let fixture: ComponentFixture<RouterComponent>;
-//
-//   beforeEach(waitForAsync(() => {
-//     TestBed.configureTestingModule({
-//       imports: [HttpClientTestingModule, RouterTestingModule],
-//       declarations: [ RouterComponent ]
-//     })
-//       .compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(RouterComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {SettingsComponent} from "./settings.component";
+import {DictionaryURLs} from "../../types";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+
+describe('SettingsComponent', () => {
+  let component: SettingsComponent;
+  let fixture: ComponentFixture<SettingsComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SettingsComponent ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SettingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should do a thing?', () => {
+
+    const invalidUrls: DictionaryURLs = {
+      leadmineURL: "",
+      compoundConverterURL: "",
+      unichemURL: "",
+    }
+
+    expect(component.validURLs(invalidUrls)).toBeFalse()
+
+  });
+});
