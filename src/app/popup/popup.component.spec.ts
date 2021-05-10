255477cf4c3540ff90029934646e4227a501c1de
Ian Dunlop <ian.dunlop@md.catapult.org.uk>
2021-05-10 16:07:30 +0100

    tslint --fix sorts out some linting

--- src/app/popup/popup.component.spec.ts
+++ src/app/popup/popup.component.spec.ts
@@ -8,7 +8,7 @@ describe('PopupComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-    declarations: [ 
+    declarations: [
         PopupComponent,
         MockSettingsComponent
     ]})
