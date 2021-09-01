59dd62ae205e41f641864560e3fe99f8d0cf385c
Alex Whitehorn <alex.whitehorn@md.catapult.org.uk>
2021-09-01 13:50:51 +0100

    fix tests

--- src/app/pdfselector/pdfselector.component.spec.ts
+++ src/app/pdfselector/pdfselector.component.spec.ts
@@ -1,22 +1,24 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LogService } from '../popup/log.service';
+import { TestBrowserService } from '../test-browser.service';
 
 import { PDFSelectorComponent } from './pdfselector.component';
 
 describe('PDFSelectorComponent', () => {
   let component: PDFSelectorComponent;
-  let fixture: ComponentFixture<PDFSelectorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ PDFSelectorComponent ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PDFSelectorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    const httpMock = TestBed.get(HttpClientTestingModule)
+    component = new PDFSelectorComponent(new LogService(), httpMock, new TestBrowserService(new LogService()))
   });
 
   it('should create', () => {
