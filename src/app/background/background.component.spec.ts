d37f62ae0e9f6f0050cad020b2ccfb8f1e4ade6f
Alex Whitehorn <alex.whitehorn@md.catapult.org.uk>
2021-10-07 15:32:30 +0100

    Add unique entity tests

--- src/app/background/background.component.spec.ts
+++ src/app/background/background.component.spec.ts
@@ -39,46 +39,49 @@ describe('BackgroundComponent', () => {
   });
 
   it('should create unique entities', () => {
-    const leadmineEntities: Array<LeadminerEntity> = ['protein', 'protein', 'sometext'].map(entity => {
-      const leadmineEntity =  {
-      beg: 0,
-      begInNormalizedDoc: 0,
-      end: 0,
-      endInNormalizedDoc: 0,
-      entityGroup: '',
-      entityText: entity,
-      possiblyCorrectedText: '',
-      recognisingDict: undefined,
-      resolvedEntity: '',
-      sectionType: ''
-    }
-      return leadmineEntity;
-    })
+    const leadmineEntities = getLeadminerEntities(['protein', 'protein', 'sometext'])
     const result = backgroundComponent.getUniqueEntities(leadmineEntities);
     expect(result.length).toBe(2);
   });
 
-  it('should return nothing if settings min entity length is higher than entity length', () => {
-    const leadmineEntities: Array<LeadminerEntity> = ['pro', 'pro', 'some'].map(entity => {
-      const leadmineEntity =  {
-      beg: 0,
-      begInNormalizedDoc: 0,
-      end: 0,
-      endInNormalizedDoc: 0,
-      entityGroup: '',
-      entityText: entity,
-      possiblyCorrectedText: '',
-      recognisingDict: undefined,
-      resolvedEntity: '',
-      sectionType: ''
-    }
-      return leadmineEntity;
+  it('unique entities should filter entities whose length is less than minEntityLength', () => {
+    const leadmineEntities = getLeadminerEntities(['foo', 'bar', 'some'])
+    const tests = [{
+      minEntityLength: 5,
+      expected: 0,
+    }, {
+      minEntityLength: 4,
+      expected: 1,
+    }, {
+      minEntityLength: 3,
+      expected: 3
+    }, {
+      minEntityLength: 0,
+      expected: 3,
+    }]
+    tests.forEach(test => {
+      backgroundComponent.settings.preferences.minEntityLength = test.minEntityLength
+      const result = backgroundComponent.getUniqueEntities(leadmineEntities);
+      expect(result.length).toBe(test.expected);
     })
+  })
 
-    backgroundComponent.settings.preferences.minEntityLength = 5
-    const result = backgroundComponent.getUniqueEntities(leadmineEntities);
-    expect(result.length).toBe(0);
-  });
-});
+  function getLeadminerEntities(entities: string[]): LeadminerEntity[] {
+    return entities.map(entity => {
+      return {
+        beg: 0,
+        begInNormalizedDoc: 0,
+        end: 0,
+        endInNormalizedDoc: 0,
+        entityGroup: '',
+        entityText: entity,
+        possiblyCorrectedText: '',
+        recognisingDict: undefined,
+        resolvedEntity: '',
+        sectionType: ''
+      }
+    })
+  }
+})
 
 
