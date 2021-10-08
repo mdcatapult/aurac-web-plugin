0b89b94a6b2b3a78a0dc39ab9cd41c3ca94d7614
Brad <brad.rollings@md.catapult.org.uk>
2021-10-08 11:11:02 +0100

    refactored test names

--- spec/textHighlighter.spec.ts
+++ spec/textHighlighter.spec.ts
@@ -7,90 +7,90 @@ describe('textContainsTerm', () => {
     const terms = ['protein', 'Glucans biosynthesis protein D']
 
     terms.forEach(term => {
-      it('term is text', () => {
+      it('returns true given the same text and term', () => {
         let result = TextHighlighter.textContainsTerm(term, term)
         expect(result).toBe(true);
       });
 
-      it('term not in text', () => {
+      it('returns false given the term isnt the same as the text', () => {
         let result = TextHighlighter.textContainsTerm('term', term)
         expect(result).toBe(false);
       });
 
-      it('multiple terms surrounded by whitespace within text', () => {
+      it('returns true given multiple terms which are surrounded by whitespace within text', () => {
         let result = TextHighlighter.textContainsTerm(` ${term} ${term} ${term} `, term)
         expect(result).toBe(true);
       });
 
-      it('whitespace within term', () => {
+      it('returns false given there is whitespace within a term', () => {
         let result = TextHighlighter.textContainsTerm('pro tein', term)
         expect(result).toBe(false);
       });
 
-      it('empty text', () => {
+      it('returns false given there is empty text', () => {
         let result = TextHighlighter.textContainsTerm(' ', term)
         expect(result).toBe(false);
       });
 
-      it('term enclosed in characters which are not whitespace or delimiters', () => {
+      it('returns false given a term enclosed in characters which are not whitespace or delimiters', () => {
         let result = TextHighlighter.textContainsTerm(`text${term}text`, term)
         expect(result).toBe(false);
       });
 
-      it('term at the end of text and not preceded by whitespace', () => {
+      it('returns false given a term at the end of text and not preceded by whitespace', () => {
         let result = TextHighlighter.textContainsTerm(`text${term}`, term)
         expect(result).toBe(false);
       });
 
-      it('term at the end of text and preceded by whitespace', () => {
+      it('returns true given a term at the end of text and preceded by whitespace', () => {
         let result = TextHighlighter.textContainsTerm(`text ${term}`, term)
         expect(result).toBe(true);
       });
 
-      it('term at the start of text and not followed by whitespace', () => {
+      it('returns false given a term at the start of text and not followed by whitespace', () => {
         let result = TextHighlighter.textContainsTerm(`${term}text`, term)
         expect(result).toBe(false);
       });
 
-      it('term at the start of text and followed by whitespace', () => {
+      it('returns true given a term at the start of text and followed by whitespace', () => {
         let result = TextHighlighter.textContainsTerm(`${term} text`, term)
         expect(result).toBe(true);
       });
 
-      it('text preceding term ends with delimiter and text following term starts with whitespace', () => {
+      it('returns true given a text preceding term ends with delimiter and text following term starts with whitespace', () => {
         TextHighlighter.delimiters.forEach(delimiter => {
           let result = TextHighlighter.textContainsTerm(`text${delimiter}${term} text`, term)
           expect(result).toBe(true);
         })
       });
 
-      it('text preceding term ends with delimiter and term is at end of text', () => {
+      it('returns true given a text preceding term ends with delimiter and term is at end of text', () => {
         TextHighlighter.delimiters.forEach(delimiter => {
           let result = TextHighlighter.textContainsTerm(`text${delimiter}${term}`, term)
           expect(result).toBe(true);
         })
       });
 
-      it('text starts and ends with the same delimiter', () => {
+      it('returns true given a text starts and ends with the same delimiter', () => {
         TextHighlighter.delimiters.forEach(delimiter => {
           let result = TextHighlighter.textContainsTerm(`${delimiter}${term}${delimiter}`, term)
           expect(result).toBe(true);
         })
       });
 
-      it('text starts and ends with different delimiters', () => {
+      it('returns true given a text starts and ends with different delimiters', () => {
         let result = TextHighlighter.textContainsTerm(`(${term})`, term)
         expect(result).toBe(true);
       })
 
-      it('text following term starts with delimiter and text preceding term ends with whitespace', () => {
+      it('returns true given a text following a term starts with delimiter and text preceding term ends with whitespace', () => {
         TextHighlighter.delimiters.forEach(delimiter => {
           let result = TextHighlighter.textContainsTerm(` ${term}${delimiter} `, term)
           expect(result).toBe(true);
         })
       });
 
-      it('text following term starts with delimiter and term is at start of text', () => {
+      it('returns true given a text following a term starts with delimiter and term is at start of text', () => {
         TextHighlighter.delimiters.forEach(delimiter => {
           let result = TextHighlighter.textContainsTerm(`${term}${delimiter}information`, term)
           expect(result).toBe(true);
@@ -115,7 +115,7 @@ describe('allTextNodes', () => {
   })
 
 
-  it('text nodes array should populate when child node has a sub tag type', () => {
+  it('given text nodes array should populate when child node has a sub tag type', () => {
     const parentNodeElement = document.createElement('div');
     const subNodeElement = document.createElement('sub')
     parentNodeElement.appendChild(subNodeElement)
@@ -126,7 +126,7 @@ describe('allTextNodes', () => {
   })
 
 
-  it('text nodes array should contain correctly reconstructed chemical formula', () => {
+  it('given text nodes array should contain correctly reconstructed chemical formula', () => {
     const parentNodeElement = document.createElement('div');
     const subNodeElement = document.createElement('span')
     subNodeElement.innerHTML = `C<sub>17</sub>H<sub>25</sub>Br<sub>2</sub>NO<sub>2</sub>`
@@ -137,7 +137,7 @@ describe('allTextNodes', () => {
     expect(textNodes[0]).toEqual('C17H25Br2NO2\n')
   })
 
-  it('text nodes array should populate when child node has text node type', () => {
+  it('given text nodes array should populate when child node has text node type', () => {
     const parentNodeElement = document.createElement('div');
     const textNode = document.createTextNode('textNode')
     parentNodeElement.appendChild(textNode)
@@ -147,7 +147,7 @@ describe('allTextNodes', () => {
     expect(textNodes.length).toBe(1)
   })
 
-  it('text nodes array should not populate when child node has a comment node type', () => {
+  it('given text nodes array should not populate when child node has a comment node type', () => {
     const nodeElement = document.createElement('div')
     const commentNode = document.createComment('commentNode')
     nodeElement.appendChild(commentNode)
@@ -157,7 +157,7 @@ describe('allTextNodes', () => {
     expect(textNodes.length).toBe(0)
   })
 
-  it('text nodes array should not populate when child node has a processing instruction node type', () => {
+  it('given text nodes array should not populate when child node has a processing instruction node type', () => {
     const parentElement = document.createElement('div')
     const processingInstructionNode = document.createProcessingInstruction('test', 'test')
     parentElement.appendChild(processingInstructionNode)
@@ -167,7 +167,7 @@ describe('allTextNodes', () => {
     expect(textNodes.length).toBe(0)
   })
 
-  it('text nodes array should remain empty when passed node is a script element', () => {
+  it('given text nodes array should remain empty when passed node is a script element', () => {
     const parentElement = document.createElement('div')
     const scriptElement = document.createElement('script')
     parentElement.appendChild(scriptElement)
@@ -177,7 +177,7 @@ describe('allTextNodes', () => {
     expect(textNodes.length).toBe(0)
   })
 
-  it('text nodes array should remain empty when passed node is a style element', () => {
+  it('given text nodes array should remain empty when passed node is a style element', () => {
     const parentElement = document.createElement('div')
     const styleElement = document.createElement('style')
     parentElement.appendChild(styleElement)
@@ -187,7 +187,7 @@ describe('allTextNodes', () => {
     expect(textNodes.length).toBe(0)
   })
 
-  it('text nodes array should remain empty when passed node is an SVG element', () => {
+  it('given text nodes array should remain empty when passed node is an SVG element', () => {
     const parentElement = document.createElement('div')
     const svgElement = document.createElement('svg')
     parentElement.appendChild(svgElement)
@@ -197,7 +197,7 @@ describe('allTextNodes', () => {
     expect(textNodes.length).toBe(0)
   })
 
-  it('text nodes array should remain empty when passed node is an input element', () => {
+  it('given text nodes array should remain empty when passed node is an input element', () => {
     const parentElement = document.createElement('div')
     const inputElement = document.createElement('input')
     parentElement.appendChild(inputElement)
@@ -207,7 +207,7 @@ describe('allTextNodes', () => {
     expect(textNodes.length).toBe(0)
   })
 
-  it('text nodes array should remain empty when passed node is a button element', () => {
+  it('given text nodes array should remain empty when passed node is a button element', () => {
     const parentElement = document.createElement('div')
     const buttonElement = document.createElement('button')
     parentElement.appendChild(buttonElement)
@@ -217,7 +217,7 @@ describe('allTextNodes', () => {
     expect(textNodes.length).toBe(0)
   })
 
-  it('text nodes array should remain empty when passed node is an anchor element', () => {
+  it('given text nodes array should remain empty when passed node is an anchor element', () => {
     const parentElement = document.createElement('div')
     const anchorElement = document.createElement('anchor')
     parentElement.appendChild(anchorElement)
@@ -227,7 +227,7 @@ describe('allTextNodes', () => {
     expect(textNodes.length).toBe(0)
   })
 
-  it('should not populate text nodes array with sidebar element', () => {
+  it('given text nodes array should not populate text nodes array with sidebar element', () => {
     const parentElement = document.createElement('div')
     const sidebarElement = document.createElement('span')
     sidebarElement.textContent = 'something'
@@ -239,7 +239,7 @@ describe('allTextNodes', () => {
     expect(textNodes.length).toBe(0)
   })
 
-  it('should not populate text nodes array with hidden element', () => {
+  it('given text next nodes should not populate text nodes array with hidden element', () => {
     const parentElement = document.createElement('div')
     const hiddenElement = document.createElement('p')
     hiddenElement.textContent = 'something'
@@ -251,7 +251,7 @@ describe('allTextNodes', () => {
     expect(textNodes.length).toBe(0)
   })
 
-  it('should not populate text nodes array with element of tooltipped class', () => {
+  it('given text nodes array should not populate text nodes array with element of tooltipped class', () => {
     const parentElement = document.createElement('div')
     const hiddenElement = document.createElement('p')
     hiddenElement.textContent = 'something'
@@ -263,7 +263,7 @@ describe('allTextNodes', () => {
     expect(textNodes.length).toBe(0)
   })
 
-  it('should not populate text nodes array with element of tooltipped-click class', () => {
+  it('given text nodes array should not populate text nodes array with element of tooltipped-click class', () => {
     const parentElement = document.createElement('div')
     const hiddenElement = document.createElement('p')
     hiddenElement.textContent = 'something'
@@ -290,7 +290,7 @@ describe('allDescendants', () => {
     elements = []
   })
 
-  it('should populate elements array with a single valid descendant', () => {
+  it('returns array length of 1 when populating elements array with a single valid descendant', () => {
     const parentElement = document.createElement('div')
     const textElement = document.createElement('p')
     textElement.textContent = 'protein'
@@ -301,7 +301,7 @@ describe('allDescendants', () => {
     expect(elements.length).toBe(1);
   })
 
-  it('should populate elements array with multiple valid descendants', () => {
+  it('should populate elements array with a couple of valid descendants', () => {
     const parentElement = document.createElement('div')
     const textElement = document.createElement('p')
     textElement.textContent = 'protein'
@@ -316,7 +316,7 @@ describe('allDescendants', () => {
     expect(elements.length).toBe(2);
   })
 
-  it('should only populate elements array with descendants which are text nodes', () => {
+  it('should populate elements array with descendants which are allowed', () => {
     const parentElement = document.createElement('div')
     const textElement = document.createElement('p')
     textElement.textContent = 'protein'
@@ -331,6 +331,17 @@ describe('allDescendants', () => {
     TextHighlighter.allDescendants(parentElement, elements, 'protein')
 
     expect(elements.length).toBe(2);
+  })
+
+  it('should populate elements array with descendants which are text nodes and term is within text', () => {
+    const parentElement = document.createElement('div')
+    const textNode = document.createTextNode('protein')
+
+    parentElement.appendChild(textNode)
+
+    TextHighlighter.allDescendants(parentElement, elements, 'protein')
+
+    expect(elements.length).toBe(1);
   })
 
   it('should ignore descendants of class aurac-sidebar', () => {
@@ -358,10 +369,9 @@ describe('allDescendants', () => {
 
     expect(elements.length).toBe(0);
   })
-
 })
 
-it('should remove highlight', () => {
+it('should remove highlights when we call removeHighlights method', () => {
     let document: Document;
     document = setup([])
 
@@ -384,7 +394,7 @@ describe('elementHasHighlightParents', () => {
     document = setup([])
   })
 
-  it('should return true if direct parent of passed element is an aurac highlight', () => {
+  it('returns true if direct parent of passed element is an aurac highlight', () => {
     const parentElement = document.createElement('div')
     parentElement.className = TextHighlighter.highlightClass
     const childElement = document.createElement('p')
@@ -393,7 +403,7 @@ describe('elementHasHighlightParents', () => {
     expect(TextHighlighter.elementHasHighlightedParents(childElement)).toBe(true)
   })
 
-  it('should return true if grandparent of passed element is an aurac highlight', () => {
+  it('returns true if grandparent of passed element is an aurac highlight', () => {
     const grandParentElement = document.createElement('div')
     grandParentElement.className = TextHighlighter.highlightClass
     const parentElement = document.createElement('div')
@@ -404,7 +414,7 @@ describe('elementHasHighlightParents', () => {
     expect(TextHighlighter.elementHasHighlightedParents(childElement)).toBe(true)
   })
 
-  it('should return false if passed element has no aurac highlight parent', () => {
+  it('returns false if passed element has no aurac highlight parent', () => {
     const grandParentElement = document.createElement('div')
     const parentElement = document.createElement('div')
     grandParentElement.appendChild(parentElement)
@@ -415,7 +425,7 @@ describe('elementHasHighlightParents', () => {
   })
 })
 
-it('addHighlightAndEventListeners should highlight an entity', () => {
+it('should highlight an entity when we call addHighlightAndEventListeners method', () => {
     const document = setup([])
     const elementToBeHighlighted = document.createElement('p')
     elementToBeHighlighted.id = 'elementToBeHighlighted'
