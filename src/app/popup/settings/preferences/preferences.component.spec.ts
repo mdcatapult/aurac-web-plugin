0ce8df56da05edb55f321d143198d3b34954d8bd
Alex Whitehorn <alex.whitehorn@md.catapult.org.uk>
2021-11-26 11:51:37 +0000

    fix tests

--- src/app/popup/settings/preferences/preferences.component.spec.ts
+++ src/app/popup/settings/preferences/preferences.component.spec.ts
@@ -1,17 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-
 import { PreferencesComponent } from './preferences.component'
-import { FormBuilder, FormControl } from '@angular/forms'
-import { defaultSettings } from 'src/types/settings'
 import { RecogniserNamePipe } from '../recogniser-name.pipe'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { BrowserService } from 'src/app/browser.service'
+import { TestBrowserService } from 'src/app/test-browser.service'
 
 describe('PreferencesComponent', () => {
   let component: PreferencesComponent
   let fixture: ComponentFixture<PreferencesComponent>
-  const fb = new FormBuilder()
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: BrowserService, useClass: TestBrowserService }],
       declarations: [PreferencesComponent, RecogniserNamePipe]
     }).compileComponents()
   })
